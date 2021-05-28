ce7d8dc4c73ebed8b37a8967e7dd818178de2530
z80dev <dev@millenial.finance>
2021-05-28 00:11:59 -0400

    Add compiled cross-chain zaps

---
+++ resources/public/precache-manifest.a779aa737e1c4629833984098d124d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a0ddf5d9b41142ebf4c656dd637070",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb10486c2ebc440fa361",
+    "url": "./static/css/main.d3d48ca1.chunk.css"
+  },
+  {
+    "revision": "f2b1c881369923d668f9",
+    "url": "./static/js/2.fe51e08a.chunk.js"
+  },
+  {
+    "revision": "169289b1f5ea62f24aece21a6e510e51",
+    "url": "./static/js/2.fe51e08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb10486c2ebc440fa361",
+    "url": "./static/js/main.da2921b7.chunk.js"
+  },
+  {
+    "revision": "29b56fc9fdf8ab5b60ae",
+    "url": "./static/js/runtime-main.de14b5ff.js"
+  }
+]);
